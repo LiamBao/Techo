c9be057761d7f39837dbb2b123ab0ede4315754d
kaibao <kaibao@cisco.com>
2018-11-14 11:20:29 +0800

    include spring microservice

---
+++ SpringBoot-Dubbo-Zookeeper.md
@@ -0,0 +1,115 @@
+###start up
+>   redis-server redis.conf
+>   nohup ./src/redis-server redis.conf &
+killall redis-server
+
+    brew install zookeeper
+    To have launchd start zookeeper now and restart at login:
+    brew services start zookeeper
+    Or, if you don't want/need a background service you can just run:
+    zkServer start
+
+/usr/local/etc/zookeeper/zoo.cfg
+
+#### compile:
+mvn clean source:jar install -Dmaven.test.skip
+mvn clean package -Dmaven.test.skip=true //huhu
+
+IDE 支持
+使用以下命令来生成 IDE 的工程
+
+Intellij Idea
+mvn idea:idea
+eclipse
+mvn eclipse:eclipse
+
+mvn spring-boot:run
+- redis.conf
+```
+daemonize yes
+pidfile /usr/local/Cellar/redis/4.0.9/redis.pid
+port 6379
+timeout 300
+loglevel debug
+logfile /Users/liambao/Desktop/bitbuckets/huhuserver/logs/log-redis.log
+databases 16
+save 900 1
+save 300 10
+save 60 10000
+rdbcompression yes
+dbfilename dump.rdb
+dir /Users/liambao/Desktop/bitbuckets/huhuserver/logs/
+appendonly no
+appendfsync everysec
+stop-writes-on-bgsave-error no
+```
+
+- zoo.conf
+```
+# The number of milliseconds of each tick
+tickTime=2000
+# The number of ticks that the initial 
+# synchronization phase can take
+initLimit=10
+# The number of ticks that can pass between 
+# sending a request and getting an acknowledgement
+syncLimit=5
+# the directory where the snapshot is stored.
+# do not use /tmp for storage, /tmp here is just 
+# example sakes
+dataDir=/Users/ruihuan/Documents/02MyPrivate/02Star/zookeeper-3.4.12/data
+# the port at which the clients will connect
+clientPort=2181
+# the maximum number of client connections.
+# increase this if you need to handle more clients
+#maxClientCnxns=60
+#
+# Be sure to read the maintenance section of the 
+# administrator guide before turning on autopurge.
+#
+# http://zookeeper.apache.org/doc/current/zookeeperAdmin.html#sc_maintenance
+#
+# The number of snapshots to retain in dataDir
+#autopurge.snapRetainCount=3
+# Purge task interval in hours
+# Set to "0" to disable auto purge feature
+#autopurge.purgeInterval=1
+```
+
+
+
+https://zhuanlan.zhihu.com/p/25862694
+###Springboot 整合 Dubbo/ZooKeeper 详解 SOA 案例
+
+Zookeeper : service registration and load balancce
+Dobbu : 管理中间层工具,在业务层到数据仓库间有非常多服务的接入和服务提供者需要调度，dubbo提供一个框架解决这个问题
+
+zookeeper和dubbo的关系：
+Dubbo的将注册中心进行抽象，是得它可以外接不同的存储媒介给注册中心提供服务，有ZooKeeper，Memcached，Redis
+也就把ZooKeeper的特性引进来。首先是负载均衡，单注册中心的承载能力是有限的，在流量达到一定程度的时候就需要分流，负载均衡就是为了分流而存在的，一个ZooKeeper群配合相应的Web应用就可以很容易达到负载均衡；资源同步，单单有负载均衡还不够，节点之间的数据和资源需要同步，ZooKeeper集群就天然具备有这样的功能；命名服务，将树状结构用于维护全局的服务地址列表，服务提供者在启动的时候，向ZK上的指定节点/dubbo/${serviceName}/providers目录下写入自己的URL地址，这个操作就完成了服务的发布。其他特性还有Mast选举，分布式锁等。
+
+
+## Zookeeper
+https://mp.weixin.qq.com/s/rMl60YKnSVL5GfkXU8p9Ow
+ZooKeeper 是一个开源的分布式协调服务，ZooKeeper框架最初是在“Yahoo!"上构建的，用于以简单而稳健的方式访问他们的应用程序。 
+后来，Apache ZooKeeper成为Hadoop，HBase和其他分布式框架使用的有组织服务的标准。 
+例如，Apache HBase使用ZooKeeper跟踪分布式数据的状态。ZooKeeper 的设计目标是将那些复杂且容易出错的分布式一致性服务封装起来，
+构成一个高效可靠的原语集，并以一系列简单易用的接口提供给用户使用。
+>原语： 操作系统或计算机网络用语范畴。是由若干条指令组成的，用于完成一定功能的一个过程。具有不可分割性·即原语的执行必须是连续的，在执行过程中不允许被中断。
+
+ZooKeeper 是一个典型的分布式数据一致性解决方案，分布式应用程序可以基于 ZooKeeper 实现诸如数据发布/订阅、负载均衡、命名服务、分布式协调/通知、集群管理、Master 选举、分布式锁和分布式队列等功能。
+
+Zookeeper 一个最常用的使用场景就是用于担任服务生产者和服务消费者的注册中心。 服务生产者将自己提供的服务注册到Zookeeper中心，服务的消费者在进行服务调用的时候先到Zookeeper中查找服务，获取到服务生产者的详细信息之后，再去调用服务生产者的内容与数据。如下图所示，在 Dubbo架构中 Zookeeper 就担任了注册中心这一角色
+
+- ZooKeeper  本身就是一个分布式程序（只要半数以上节点存活，ZooKeeper  就能正常服务）。
+
+- 为了保证高可用，最好是以集群形态来部署 ZooKeeper，这样只要集群中大部分机器是可用的（能够容忍一定的机器故障），那么 ZooKeeper 本身仍然是可用的。
+
+- ZooKeeper  将数据保存在内存中，这也就保证了 高吞吐量和低延迟（但是内存限制了能够存储的容量不太大，此限制也是保持znode中存储的数据量较小的进一步原因
+
+- ZooKeeper 是高性能的。 在“读”多于“写”的应用程序中尤其地高性能，因为“写”会导致所有的服务器间同步状态。（“读”多于“写”是协调服务的典型场景。）
+
+- ZooKeeper有临时节点的概念。 当创建临时节点的客户端会话一直保持活动，瞬时节点就一直存在。而当会话终结时，瞬时节点被删除。持久节点是指一旦这个ZNode被创建了，除非主动进行ZNode的移除操作，否则这个ZNode将一直保存在Zookeeper上。
+
+- ZooKeeper 底层其实只提供了两个功能：①管理（存储、读取）用户程序提交的数据；②为用户程序提交数据节点监听服务
+
---
+++ learning.sh
@@ -0,0 +1,159 @@
+#!/usr/bin/env bash
+mkdir -p $logdir > /dev/null 2>&1
+bash -x ./build.sh $CONFIGFILE $2 > $logdir/build-heb-vms-$ts.log 2>&1
+
+
+
+# Remove cached SSH keys for the vm, new RSA keypair will be used.
+egrep -v "$6" /root/.ssh/known_hosts > /root/.ssh/known_hosts.new
+mv /root/.ssh/known_hosts.new /root/.ssh/known_hosts
+exp_cmd="set timeout 20
+spawn ssh root\@$6;
+expect {
+\"yes/no\"   { send \"yes\r\" ; exp_continue }
+\"Password:\" { send \"$root_pwd\r\" }
+\"password:\" { send \"$root_pwd\r\" }
+timeout       { exit }
+}
+expect \"Router#\"
+send \"show ip int brief\r\"
+expect \"Router#\"
+send \"exit\r\"
+interact"
+result="$(expect -c "$exp_cmd")"
+echo "$result" >> $setuplog
+var="$(echo $result | grep "show ip int")"
+if [ -z "$var" ]; then
+    printf "$cmd_fail - failed to set initial config to vm"
+else
+    printf "$cmd_succ"
+fi
+
+
+##   REGX
+
+pid="$(sed -n -e 's/^\*[0-9]\s*\(\S*\).*/\1/p' $router_prov | head -n 1)”
+serial_number="$(sed -n -e 's/^\*[0-9]\s*\S*\s*\([0-9A-Z]*\)\s*.*/\1/p' $router_prov | head -n 1)
+
+
+string = "\
+CGR1000_JAD1908022Y#show version | inc \*1\
+*1    CGR1120/K9            JAD1908022Y\
+CGR1000_JAD1908022Y#show version | inc \*2"
+
+
+a = sed -n -e 's/^\*[0-9]\s*\S*\s*\([0-9A-Z]*\)\s*.*/\1/p' $string  | head -n 1
+output = JAD1908022Y
+
+
+grep ER_CONSOLE_IP $3 | cut -d '=' -f 2
+sed -n 's/ER_CONSOLE_PORT=\(\S*\).*/\1/p' $3
+grep CISCO-IOK-FND /root/.install | cut -d ' ' -f 3 | tr -d '\b\r'
+grep -v   'strRegx'   file > newfile
+Tail -n I path | head -n j
+Wc -l xx.log | cut -d’ ‘ -f1
+
+
+if [ -s $File ]; then
+    echo "OK"
+fi
+
+#####################
+
+
+ssh -o TCPKeepAlive=yes -o ConnectTimeout=30 -o StrictHostKeyChecking=no -i /root/.ssh/orch_rsa 192.168.234.2 "vim-cmd vmsvc/power.on vmid”
+
+
+csr_ip="$1"
+if [ -z "$csr_ip" ]; then
+    csr_ip="$(grep "-HER\s" $orch_install| cut -d' ' -f3 -s | tr -d '\b\r')"
+fi
+
+if [ "${csr_ip:0:13}" = "CISCO-IOK-HER" ]; then
+    new_csr_ip=""
+    for f in $orch_home/CSR1000V-config-*; do
+        csr_vmname="$(fnGetProperty "$f" "VMNAME")"
+        if [ "$csr_vmname" = "$csr_ip" ]; then
+            new_csr_ip="$(fnGetProperty "$f" "CSR-INTERNAL-IP")"
+            break
+        fi
+    done
+
+    csr_ip=$new_csr_ip
+fi
+
+if [ -z "$csr_ip" ]; then
+    printf "$cmd_fail - Cannot find CSR1000V $csr_ip Internal IP address"
+    exit 1
+fi
+
+
+
+
+####  验证上条语句是否执行成功
+# vm-orch-cmd  line 2676
+### dhcp Dynamic Host Configuration Protocol Client
+
+res="$(ipcalc -c -6 $mesh_addr 2>/dev/null)"
+if [ $? -ne 0 ]; then
+    # Mesh prefix is invalid; try with mesh prefix delegation
+    if [ -f "/var/run/dhclient6.pid" ]; then
+    #dhclient -6 -r eth0
+    killall dhclient 2>/dev/null
+    fi
+
+    rm -f /tmp/${serial_number}.lease
+    echo "default-duid \"${pid}:${serial_number}\";" > /tmp/${serial_number}.lease
+    res="$(dhclient -6 -P -sf /usr/sbin/dhclientpd-script -lf /tmp/${serial_number}.lease eth0)"
+    if [ -n "$res" ]; then
+        mesh_prefix=$res
+
+        mesh_prefix_length="$(echo $mesh_prefix | cut -d'/' -f2)"
+        mesh_prefix_addr="$(echo $mesh_prefix|cut -d'/' -f1)"
+        if [[ "$mesh_prefix_addr" == *:: ]]; then
+            mesh_addr="${mesh_prefix_addr}1/${mesh_prefix_length}"
+        elif [[ "$mesh_prefix_addr" == *:0 ]]; then
+            mesh_addr="${mesh_prefix_addr%:*}:1/${mesh_prefix_length}"
+        else
+            mesh_addr=$mesh_prefix
+        fi
+
+     rm -f /tmp/${serial_number}.lease
+    else
+        printf "$cmd_fail - failed to fetch dhclient prefix6 from dhcp server"
+         rm -f /tmp/${serial_number}.lease
+        exit 1
+    fi
+fi
+#-6     Use the DHCPv6 protocol to obtain whatever IPv6 addresses are available along  with  configuration
+#parameters.   It  cannot  be combined with -4.  The -S -T -P and -N arguments provide more control
+#over aspects of the DHCPv6 processing.  Note: it is not recommended to mix  queries  of  different
+#types together or even to share the lease file between them.
+
+#-P     Enable IPv6 prefix delegation.  This implies -6 and also disables the normal address  query.   See
+#-N to restore it.  Note only one requested interface is allowed.
+
+#-sf script-file
+#Path to the  network  configuration  script  invoked  by  dhclient  when  it  gets  a  lease.   If
+#unspecified,  the  default  CLIENTBINDIR/dhclient-script  is  used.   See dhclient-script(8) for a
+#description of this file.
+#-lf lease-file
+#Path  to the lease database file.  If unspecified, the default DBDIR/dhclient.leases is used.  See
+#dhclient.leases(5) for a descriptionof this file.
+
+
+
+
+##Suppress  lines  with no delimiter characters, when used with
+#the �−f option. Unless specified,  lines  with  no  delimiters
+#shall be passed through untouched.
+
+
+
+###
+#secure copy (remote file copy program)
+    #-i
+    #Selects the file from which the identity (private key) for public key authentication is read.  This
+    #option is directly passed to ssh(1).
+
+result = "$(scp -i /root/.ssh/orch_rsa $orch_home/cgr_admin_pwd root@${nms_ip_inner}:/root/cgr_admin.pwd)"
